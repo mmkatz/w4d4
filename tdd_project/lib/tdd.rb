05c9ab3580e64b1df73c85763e528dfdf18ce258
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-21 17:43:01 -0500

    Finished testing for #my_transpose

--- tdd_project/lib/tdd.rb
+++ tdd_project/lib/tdd.rb
@@ -5,5 +5,18 @@ def my_unique(arr)
 end
 
 def two_sum(arr)
-    arr.each do |
+    indexes = []
+
+    (0...arr.length).each do |i|
+        (i+1...arr.length).each do |j|
+            indexes << [i,j] if arr[i] + arr[j] == 0
+        end 
+    end 
+
+    indexes
 end
+
+def my_transpose(arr)
+    arr.transpose
+end 
+
--- tdd_project/spec/tdd_spec.rb
+++ tdd_project/spec/tdd_spec.rb
@@ -17,6 +17,36 @@ describe "#two_sum" do
     end
         
     it "should return a 2D array with the indexes that sum to zero. Order matters, smallest index to largest, dictionary wise" do 
-        expect (two_sum([-1, 0, 2, -2, 1])).to eq([[0, 4], [2, 3]])
+        expect(two_sum([-1, 0, 2, -2, 1])).to eq([[0, 4], [2, 3]])
+        expect(two_sum([1,2,3,4])).to eq([])
     end
 end
+
+describe "#my_transpose" do 
+    let(:array_1) { [[0, 1, 2],[3, 4, 5],[6, 7, 8]] }
+
+
+    it "should accept a 2D array as an argument" do 
+        expect {my_transpose(array_1)}.to_not raise_error
+    end 
+
+    context "for even numbers" do 
+        it "should transpose the 2D array" do 
+            expect(my_transpose([[0, 1, 2],[3, 4, 5],[6, 7, 8]])).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+        end 
+    
+        it "should not use the build in Array#transpose function" do 
+            expect(arr)
+        end 
+    end 
+
+    # it "should transpose the 2D array" do 
+    #     expect(my_transpose([[0, 1, 2],[3, 4, 5],[6, 7, 8]])).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    # end 
+
+    # it "should not use the build in Array#transpose function" do 
+    #     expect(arr)
+    # end 
+
+
+end 
